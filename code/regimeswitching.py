bb5447e58ad852cf4228f4939ae6e87e36dd05ee
tayatoshi <tsk14sns@gmail.com>
2018-05-24 21:32:12 +0900

    delete comment

--- code/regimeswitching.py
+++ code/regimeswitching.py
@@ -39,8 +39,7 @@ class Regimeswitching(object):
         self.TRANCE = 0.5 * np.ones([2,2])# Pr(S_t|S_t-1)/M * M
         self.pi0 = np.array([(1 - self.TRANCE[1,1])/(2 - self.TRANCE[0,0] - self.TRANCE[1,1]),
                              (1 - self.TRANCE[0,0])/(2 - self.TRANCE[0,0] - self.TRANCE[1,1])])# [pr(s_0=0|omega_0)=1-p/2-p-q, pr(s_0=1|omega_0)=1-q/2-p-q]
-        # self.c = np.ones([self.M,self.J])# constant: M * J
-        self.c = np.array([30,60])# constant: M * J
+        self.c = np.ones([self.M,self.J])# constant: M * J
         self.F = np.ones([self.M,self.J,self.J])*0.7# M * J * J
         self.H = np.ones([self.M,self.D,self.J])# M * D * J
         self.A = np.zeros([self.M,self.D,self.K])# M * D * K
@@ -58,7 +57,6 @@ class Regimeswitching(object):
         self.marginal_probability = np.zeros([self.len,self.M]) # T*M :Pr(S_t|omega_t)
         self.marginal_likelihood = np.zeros(self.len)# f(y_t|omega_t-1)
         self.marginal_loglikelihood = np.zeros(self.len)# f(y_t|omega_t-1)
-        # self.log_likilihood = np.zeros(self.len)
         self.a_marginal = np.zeros([self.len,self.M,self.J])# a_{t|t}^{j}:T * M * J
         self.p_marginal = np.zeros([self.len,self.M,self.J,self.J])# T * M * J * J
         return print("OK")
@@ -72,36 +70,20 @@ class Regimeswitching(object):
 
     def predict(self,t):
         if t==0:
-            # for i in range(self.M):
-            #     for j in range(self.M):
-            #         self.a_predict[t,i,j] = self.c[j].reshape(self.J,1) + self.F[j] @ (self.a0[i].reshape(self.J,1) - self.c[i])#a_{1|0}:J * 1
-            #         self.p_predict[t,i,j] = self.F[j] @ self.p0[i].reshape(self.J,1) @ self.F[j].T + self.Q[j]#p_{1|0}
             for i,j in itertools.product(range(self.M),range(self.M)):
                 self.a_predict[t,i,j] = self.c[j].reshape(self.J,1) + self.F[j] @ (self.a0[i].reshape(self.J,1) - self.c[i])#a_{1|0}:J * 1
                 self.p_predict[t,i,j] = self.F[j] @ self.p0[i].reshape(self.J,1) @ self.F[j].T + self.Q[j]#p_{1|0}
         else:
-            # for i in range(self.M):
-            #     for j in range(self.M):
-            #         self.a_predict[t,i,j] = self.c[j] + self.F[j] @ (self.a_marginal[t-1,i] - self.c[i])#a_{t|t-1}:J * 1
-            #         self.p_predict[t,i,j] = self.F[j] @ self.p_marginal[t-1,i] @ self.F[j].T + self.Q[j]#p_{t|t-1}
             for i,j in itertools.product(range(self.M),range(self.M)):
                 self.a_predict[t,i,j] = self.c[j].reshape(self.J,1) + self.F[j] @ (self.a0[i].reshape(self.J,1) - self.c[i])#a_{1|0}:J * 1
                 self.p_predict[t,i,j] = self.F[j] @ self.p0[i].reshape(self.J,1) @ self.F[j].T + self.Q[j]#p_{1|0}
 
     def residuals(self,t):
-        # for i in range(self.M):
-        #     for j in range(self.M):
-        #         self.eta[t,i,j] = self.y[t].T - self.H[j] @ self.a_predict[t,i,j] - self.A[j] @ self.x[t].T# D * 1
-        #         self.f[t,i,j] = self.H[j] @ self.p_predict[t,i,j] @ self.H[j].T + self.R[j]# D * D
         for i,j in itertools.product(range(self.M),range(self.M)):
             self.eta[t,i,j] = self.y[t].T - self.H[j] @ self.a_predict[t,i,j] - self.A[j] @ self.x[t].T# D * 1
             self.f[t,i,j] = self.H[j] @ self.p_predict[t,i,j] @ self.H[j].T + self.R[j]# D * D
 
     def filtering(self,t):
-        # for i in range(self.M):
-        #     for j in range(self.M):
-        #         self.a_filter[t,i,j] = self.a_predict[t,i,j] + self.p_predict[t,i,j] @ self.H[j].T @ np.linalg.inv(self.f[t,i,j]) @ self.eta[t,i,j]
-        #         self.p_filter[t,i,j] = self.p_predict[t,i,j] @ (np.eye(self.J) - self.H[j].T @ np.linalg.inv(self.f[t,i,j]) @ self.H[j] @ self.p_predict[t,i,j].T)
         for i,j in itertools.product(range(self.M),range(self.M)):
             self.a_filter[t,i,j] = self.a_predict[t,i,j] + self.p_predict[t,i,j] @ self.H[j].T @ np.linalg.inv(self.f[t,i,j]) @ self.eta[t,i,j]
             self.p_filter[t,i,j] = self.p_predict[t,i,j] @ (np.eye(self.J) - self.H[j].T @ np.linalg.inv(self.f[t,i,j]) @ self.H[j] @ self.p_predict[t,i,j].T)
@@ -109,28 +91,16 @@ class Regimeswitching(object):
     def HamiltonFilter(self,t):
         # Pr(S_t,S_t-1|omega_t-1)
         if t == 0:
-            # for i in range(self.M):
-            #     for j in range(self.M):
-            #         self.joint_probability[t,i,j] = self.TRANCE[i,j] * self.pi0[i]
             for i,j in itertools.product(range(self.M),range(self.M)):
                 self.joint_probability[t,i,j] = self.TRANCE[i,j] * self.pi0[i]
         elif t  > 0:
-            # for i in range(self.M):
-            #     for j in range(self.M):
-            #         self.joint_probability[t,i,j] = self.TRANCE[i,j] * self.marginal_probability[t-1,i]
             for i,j in itertools.product(range(self.M),range(self.M)):
                 self.joint_probability[t,i,j] = self.TRANCE[i,j] * self.marginal_probability[t-1,i]
         # f(y_t|omega_t-1)
-        # for i in range(self.M):
-        #     for j in range(self.M):
-        #         self.marginal_likelihood[t] = self.marginal_likelihood[t] + self.get_likeihood(t,i,j) * self.joint_probability[t,i,j]
         for i,j in itertools.product(range(self.M),range(self.M)):
             self.marginal_likelihood[t] = self.marginal_likelihood[t] + self.get_likeihood(t,i,j) * self.joint_probability[t,i,j]
             self.marginal_loglikelihood[t] = self.marginal_loglikelihood[t] + self.get_likeihood(t,i,j,log=True) * self.joint_probability[t,i,j]
         # Pr(S_t,S_t-1|omega_t)
-        # for i in range(self.M):
-        #     for j in range(self.M):
-        #         self.joint_filter_probability[t,i,j] = (self.get_likeihood(t,i,j) * self.joint_probability[t,i,j])/self.marginal_likelihood[t]
         for i,j in itertools.product(range(self.M),range(self.M)):
             self.joint_filter_probability[t,i,j] = (self.get_likeihood(t,i,j) * self.joint_probability[t,i,j])/self.marginal_likelihood[t]
         #Pr(S_t|omega_t)
@@ -145,20 +115,12 @@ class Regimeswitching(object):
                     denominator = 0.0001
                 else:
                     denominator = self.marginal_probability[t,j]
-                # denominator = self.marginal_probability[t,j]
             a_molecule = np.zeros((self.a_filter[t]).shape)
-            # for ii in range(self.M):
-            #     for jj in range(self.M):
-            #         a_molecule[ii,jj] = self.a_filter[t,ii,jj] * self.joint_filter_probability[t,ii,jj]#分子
             for ii,jj in itertools.product(range(self.M),range(self.M)):
                 a_molecule[ii,jj] = self.a_filter[t,ii,jj] * self.joint_filter_probability[t,ii,jj]#分子
             self.a_marginal[t,j] = np.sum(a_molecule,axis=0)[j]/denominator# a_{t|t}^{j}
 
             p_molecule = np.zeros((self.p_filter[t]).shape)
-            # for ii in range(self.M):
-            #     for jj in range(self.M):
-            #         p_molecule[ii,jj] = self.joint_filter_probability[t,ii,jj] * \
-            #                             (self.p_filter[t,ii,jj] + (self.a_filter[t,ii,jj] - self.a_marginal[t,jj]) @ (self.a_filter[t,ii,jj] - self.a_marginal[t,jj]).T)
             for ii,jj in itertools.product(range(self.M),range(self.M)):
                 p_molecule[ii,jj] = self.joint_filter_probability[t,ii,jj] * \
                                     (self.p_filter[t,ii,jj] + (self.a_filter[t,ii,jj] - self.a_marginal[t,jj]) @ (self.a_filter[t,ii,jj] - self.a_marginal[t,jj]).T)
@@ -205,8 +167,6 @@ class Regimeswitching(object):
         # return self.eta
 
     def par_opt(self):
-        # pp=np.random.uniform(0,5)
-        # qq=np.random.uniform(0,5)
         pp = 1
         qq = 1
         c0 = 30
